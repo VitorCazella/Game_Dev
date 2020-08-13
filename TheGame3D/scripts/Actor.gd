af462fb4d2615a138a57ba1030d659f54ee1e08f
Vitor Antunes Cazella <91050051@learn.cpp.ac.nz>
2020-08-13 12:38:15 +1200

    added gravity

--- TheGame3D/scripts/Actor.gd
+++ TheGame3D/scripts/Actor.gd
@@ -9,11 +9,48 @@ var direction = Vector3()
 
 const SPEED = 20
 const ACCEL = 2
+const GRAVITY = -(9.8 * 3)
 
 func _ready():
 	pass
 
 func _physics_process(delta):
+	walk(delta)
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		$head.rotate_y(deg2rad(-event.relative.x * view_sensitivity))
+		
+		var change = -event.relative.y * view_sensitivity
+		if change + camera_angle < 90 and change + camera_angle > -90:
+			$head/Camera.rotate_x(deg2rad(change))
+			camera_angle += change
+
+func walk(delta):
+	direction = Vector3()
+	
+	var aim = $head/Camera.get_global_transform().basis
+	if Input.is_action_pressed("move_forward"):
+		direction -= aim.z
+	if Input.is_action_pressed("move_backward"):
+		direction += aim.z
+	if Input.is_action_pressed("move_left"):
+		direction -= aim.x
+	if Input.is_action_pressed("move_right"):
+		direction += aim.x
+	
+	direction = direction.normalized()
+	
+	velocity.y += GRAVITY * delta
+	
+	var target = direction* SPEED
+	
+	velocity = velocity.linear_interpolate(target, ACCEL * delta)
+	
+	move_and_slide(velocity)
+	
+func fly(delta):
 	direction = Vector3()
 	
 	var aim = $head/Camera.get_global_transform().basis
@@ -33,13 +70,3 @@ func _physics_process(delta):
 	velocity = velocity.linear_interpolate(target, ACCEL * delta)
 	
 	move_and_slide(velocity)
-
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		$head.rotate_y(deg2rad(-event.relative.x * view_sensitivity))
-		
-		var change = -event.relative.y * view_sensitivity
-		if change + camera_angle < 90 and change + camera_angle > -90:
-			$head/Camera.rotate_x(deg2rad(change))
-			camera_angle += change
