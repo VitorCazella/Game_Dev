37f7eec1a3b61e58c30ca6284f176761a98c70f5
VitorCazella_PC <vitor.cazella@hotmail.com>
2020-08-17 22:58:49 +1200

    class 17/08

--- HoppyDays/src/Player.gd
+++ HoppyDays/src/Player.gd
@@ -4,17 +4,18 @@ onready var UP = Vector2(0,-1)
 
 const GRAVITY = 20
 const MAXFALLSPEED = 1000
-const SPEED = 300
-const JUMP_HEIGHT = 500
+const SPEED = 500
+const JUMP_HEIGHT = 800
 
 var motion = Vector2.ZERO
 
 func _physics_process(delta):
 	exit()
 	move()
+	animate()
 	jump()
 	gravity()
-	motion = move_and_slide(motion)
+	motion = move_and_slide(motion, UP)
 	pass
 
 func move():
@@ -24,6 +25,18 @@ func move():
 		motion.x = SPEED
 	else:
 		motion.x = 0
+
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x > 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = false
+	elif motion.x < 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.play("stand")
 
 func gravity():
 	motion.y += GRAVITY
