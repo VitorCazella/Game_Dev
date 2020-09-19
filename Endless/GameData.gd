c88bfa791f5cd301dcf91976c9a0198dff84760c
VitorCazella_PC <vitor.cazella@hotmail.com>
2020-09-19 21:10:57 +1200

    asteroids working with manual path

--- Endless/GameData.gd
+++ Endless/GameData.gd
@@ -4,6 +4,5 @@ onready var label = $HUD/Control/Label
 
 var score = 0
 
-
 func _process(delta):
 	label.text = ("Score: " + String(score))
--- Endless/src/Enemy.gd
+++ Endless/src/Enemy.gd
@@ -1,47 +1,15 @@
-extends KinematicBody2D
+extends Node2D
 
-onready var player = get_node("../../..")
-
-signal despawn
-
-var speed = 300
-var angular_speed = 0
-var _angle_change_timer = null
+export (NodePath) var path
+onready var player = get_node(path)
 
 
 func _ready():
-	# Create a timer to make the node change directions regularly
-	_angle_change_timer = Timer.new()
-	_angle_change_timer.set_wait_time(5)
-	_angle_change_timer.set_one_shot(false)
-	_angle_change_timer.connect("timeout", self, "_change_direction")
-	add_child(_angle_change_timer)
-	_angle_change_timer.start()
-
-	_change_direction()
 	pass
 
-
 func _physics_process(delta):
-	# Rotate
-	rotation = (rotation + angular_speed * delta)
-	# Move
-	var forward = angle_to_direction(rotation)
-	position = (position + forward * speed * delta)
-	
-#	if !get_node("VisibilityNotifier2D").is_on_screen():
-#		emit_signal("despawn")
-#		queue_free()
-
-
-func _change_direction():
-	angular_speed = rand_range(-PI, PI)
-
-
-static func angle_to_direction(angle):
-	return Vector2(cos(angle), -sin(angle))
-
+	pass
 
 func _on_Area2D_body_entered(body):
-	#queue_free()
+	queue_free()
 	player.score += 1
